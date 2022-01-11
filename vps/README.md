7c37f6f4c90b6d615d35b0ab1949806a5321838e
gar0t0 <gar0t0@gmail.com>
2022-01-11 20:07:03 -0300

    ajustando

--- vps/README.md
+++ vps/README.md
@@ -3,21 +3,56 @@
 ### Packages list
 List of packages to be installed
 
+- [amass]("https://github.com/OWASP/Amass") - In-depth Attack Surface Mapping and Asset Discovery
 - [anew]("https://github.com/tomnomnom/anew") - A tool for adding new lines to files, skipping duplicates
+- [anti-burl]("https://github.com/tomnomnom/hacks/tree/master/anti-burl") - A collection of hacks and one-off scripts
+- [arjun]("https://github.com/s0md3v/Arjun") - HTTP parameter discovery suite. 
 - [assetfinder]("https://github.com/tomnomnom/assetfinder") - Find domains and subdomains related to a given domain
+- [axiom]("https://github.com/pry0cc/axiom") - The dynamic infrastructure framework for everybody! Distribute the workload of many different scanning tools with ease, including nmap, ffuf, masscan, nuclei, meg and many more!
+- [chaos-client]("https://github.com/projectdiscovery/chaos-client") - Go client to communicate with Chaos DNS API.
+- [dalfox]("https://github.com/hahwul/dalfox") - DalFox is an powerful open source XSS scanning tool and parameter analyzer, utility
+- [dirsearch]("https://github.com/maurosoria/dirsearch") - Web path scanner
 - [dnsx]("https://github.com/projectdiscovery/dnsx") - dnsx is a fast and multi-purpose DNS toolkit allow to run multiple DNS queries of your choice with a list of user-supplied resolvers.
 - [ffuf]("https://github.com/ffuf/ffuf") - Fast web fuzzer written in Go
+- [findomain]("https://github.com/Findomain/Findomain") - The complete solution for domain recognition. Supports screenshoting, port scan, HTTP check, data import from other tools, subdomain monitoring, alerts via Discord, Slack and Telegram, multiple API Keys for sources and much more.
+- [gau]("https://github.com/lc/gau") - Fetch known URLs from AlienVault's Open Threat Exchange, the Wayback Machine, and Common Crawl.
+- [gauplus]("https://github.com/bp0lr/gauplus") - GAU Fork with some improvements
+- [gf]("https://github.com/tomnomnom/gf") - A wrapper around grep, to help you grep for things
+- [gitdorker]("https://github.com/obheda12/GitDorker") - A Python program to scrape secrets from GitHub through usage of a large repository of dorks
+- [git-dumper]("https://github.com/arthaud/git-dumper") - A tool to dump a git repository from a website
+- [git-scanner]("https://github.com/HightechSec/git-scanner") - A tool for bug hunting or pentesting for targeting websites that have open .git repositories available in public
+- [github-search]("https://github.com/gwen001/github-search") - Tools to perform basic search on GitHub
 - [gospider]("https://github.com/jaeles-project/gospider") - Fast web spider written in Go
-- [hakrawler]("go install github.com/hakluke/hakrawler@latest") - Simple, fast web crawler designed for easy, quick discovery of endpoints and assets within a web applicatio
+- [gowitness]("https://github.com/sensepost/gowitness") - a golang, web screenshot utility using Chrome Headless
+- [hakrawler]("go install github.com/hakluke/hakrawler@latest") - Simple, fast web crawler designed for easy, quick discovery of endpoints and assets within a web application
+- [haktrails]("https://github.com/hakluke/haktrails") - Golang client for querying SecurityTrails API data
+- [httprobe]("https://github.com/tomnomnom/httprobe") - Take a list of domains and probe for working HTTP and HTTPS servers
 - [httpx]("https://github.com/projectdiscovery/httpx") - httpx is a fast and multi-purpose HTTP toolkit allows to run multiple probers using retryablehttp library, it is designed to maintain the result reliability with increased threads.
 - [interactsh-{client|server}]("https://github.com/projectdiscovery/interactsh") - An OOB interaction gathering server and client library
 - [jaeles]("https://github.com/jaeles-project/jaeles") - The Swiss Army knife for automated Web Application Testing
+- [JSScanner]("https://github.com/0x240x23elu/JSScanner") - Js File Scanner
+- kitrunner
+- linkfinder
+- mapcidr
+- massdns
+- metabigor
+- metasploit
+- naabu
 - [nikto]("https://github.com/sullo/nikto") - Nikto web server scanner
 - [notify]("https://github.com/projectdiscovery/notify") - Notify is a Go-based assistance package that enables you to stream the output of several tools (or read from a file) and publish it to a variety of supported platforms
 - [nuclei]("https://github.com/projectdiscovery/nuclei") - Fast and customizable vulnerability scanner based on simple YAML based DSL
 - [nuclei-templates]("https://github.com/projectdiscovery/nuclei-templates") - Community curated list of templates for the nuclei engine to find security vulnerabilities
+- paramspider
 - [proxify]("https://github.com/projectdiscovery/proxify") - Swiss Army knife Proxy tool for HTTP/HTTPS traffic capture, manipulation, and replay on the go.
+- qsreplace
+- shuffledns
+- sub404
 - [subfinder]("https://github.com/projectdiscovery/subfinder") - Subfinder is a subdomain discovery tool that discovers valid subdomains for websites. Designed as a passive framework to be useful for bug bounties and safe for penetration testing.
+- subjs
+- telegram-send
+- turbosearch
+- unfurl
 - [waybackurls]("https://github.com/tomnomnom/waybackurls") - Fetch all the URLs that the Wayback Machine knows about for a domain
+- wpscan
 
 ### After installation
--- vps/postinstall.sh
+++ vps/postinstall.sh
@@ -1,83 +1,137 @@
 #!/bin/bash
 ## gar0t0 - 11/01/2022
 ## script vps install tools
+## Merge with 0xtiago script -> https://github.com/0xtiago/vps_config/blob/main/run.sh
 
 ## Variables
 LOG_FILE="/root/postinstall.log"
 TOOLS_DIR="/root/tools"
 DATE=$(date "+%Y%m%d - %H:%m:%S")
 golang_package="https://go.dev/dl/go1.17.6.linux-amd64.tar.gz"
+
+## Packages
+amass="go get -v github.com/OWASP/Amass/v3/..."
 anew="go get -u github.com/tomnomnom/anew"
+antiburl="https://raw.githubusercontent.com/tomnomnom/hacks/master/anti-burl/main.go"
+arjun="pip3 install arjun"
 assetfinder="go get -u github.com/tomnomnom/assetfinder"
+axiom="bash <(curl -s https://raw.githubusercontent.com/pry0cc/axiom/master/interact/axiom-configure)"
+chaos_client="go get -v github.com/projectdiscovery/chaos-client/cmd/chaos"
+dalfox="go install github.com/hahwul/dalfox/v2@latest"
+dirsearch="git clone https://github.com/maurosoria/dirsearch.git ${TOOLS_DIR}/dirsearch"
 dnsx="go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest"
 ffuf="go get -u github.com/ffuf/ffuf"
+findomain="https://github.com/findomain/findomain/releases/latest/download/findomain-linux"
+gau="go install github.com/lc/gau/v2/cmd/gau@latest"
+gauplus="go get -u -v github.com/bp0lr/gauplus"
+gf="go get -u github.com/tomnomnom/gf"
+gitdorker="git clone https://github.com/obheda12/GitDorker.git ${TOOLS_DIR}/gitdorker"
+gitdumper="pip3 install git-dumper"
+gitscanner="git clone https://github.com/HightechSec/git-scanner ${TOOLS_DIR}/gitscanner"
+githubsearch="git clone https://github.com/gwen001/github-search.git ${TOOLS_DIR}/githubsearch"
 gospider="go get -u github.com/jaeles-project/gospider"
+gowitness="go get -u github.com/sensepost/gowitness"
 hakrawler="go install github.com/hakluke/hakrawler@latest"
+haktrails="go install -v github.com/hakluke/haktrails@latest"
+httprobe="go get -u github.com/tomnomnom/httprobe"
 httpx="go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest"
 interactsh_client="go install -v github.com/projectdiscovery/interactsh/cmd/interactsh-client@latest"
 interactsh_server="go install -v github.com/projectdiscovery/interactsh/cmd/interactsh-server@latest"
-nikto="git clone https://github.com/sullo/nikto $TOOLS_DIR/nikto"
+nikto="git clone https://github.com/sullo/nikto ${TOOLS_DIR}/nikto"
 jaeles="go get github.com/jaeles-project/jaeles"
+jsscanner="git clone https://github.com/0x240x23elu/JSScanner.git ${TOOLS_DIR}/jsscanner"
 notify="go install -v github.com/projectdiscovery/notify/cmd/notify@latest"
 nuclei="go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest"
-nuclei_templates="git clone https://github.com/projectdiscovery/nuclei-templates.git $TOOLS_DIR/nuclei-templates"
+nuclei_templates="git clone https://github.com/projectdiscovery/nuclei-templates.git ${TOOLS_DIR}/nuclei-templates"
 proxify="go install -v github.com/projectdiscovery/proxify/cmd/proxify@latest"
 subfinder="go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest"
 waybackurls="go get github.com/tomnomnom/waybackurls"
 
 ## Start script
-echo -e "====== Starting post install Script - $DATE ======\nCheck all tools after install" >> $LOG_FILE 2>&1
+echo -e "====== Starting post install Script - ${DATE} ======\nCheck all tools after install" >> ${LOG_FILE} 2>&1
 
+## Validate root user
+if [[ $(id -u) != 0 ]]; then
+    echo -e "\n[!] Install.sh need to run as root or sudoer"
+    exit 0
+fi
 ## Create tools directory
-echo -e "====== Creating $TOOLS_DIR directory - $DATE ======\nCheck all tools after install" >> $LOG_FILE 2>&1
-mkdir "$TOOLS_DIR"
+echo -e "====== Creating ${TOOLS_DIR} directory - ${DATE} ======\nCheck all tools after install" >> ${LOG_FILE} 2>&1
+mkdir "${TOOLS_DIR}"
 
 ## Pre-Req
-echo -e "====== Starting apt process - $DATE ======\n" >> $LOG_FILE 2>&1
+echo -e "====== Starting apt process - ${DATE} ======\n" >> ${LOG_FILE} 2>&1
 { apt update
-  apt install git vim wget curl perl gcc -y 
-} >> $LOG_FILE 2>&1
-echo -e "====== Ending apt process - $DATE ======\n" >> $LOG_FILE 2>&1
+  apt install git vim wget curl perl gcc jq htop zsh default-jre net-tools locate gzip zip ruby-dev tmux p7zip libpcap-dev python3-pip python3-is-python3 snap nmap sqlmap john grepcidr masscan brutespray prips fonts-powerline -y 
+} >> ${LOG_FILE} 2>&1
+echo -e "====== Ending apt process - ${DATE} ======\n" >> ${LOG_FILE} 2>&1
 
 ## go install
-echo -e "====== Starting golang install - $DATE ======\n" >> $LOG_FILE 2>&1
+echo -e "====== Starting golang install - ${DATE} ======\n" >> ${LOG_FILE} 2>&1
 { wget $golang_package -O /tmp/golang-latest.tar.gz
   tar -xf /tmp/golang-latest.tar.gz -C /usr/local
   echo 'export PATH="$PATH:/usr/local/go/bin:~/go/bin"' >> ~/.bashrc
   echo 'export GOPATH=/root/go' >> ~/.bashrc
-} >> $LOG_FILE 2>&1
+} >> ${LOG_FILE} 2>&1
 
-## Exporting go variables
+## Exporting variables
 export PATH="$PATH:/usr/local/go/bin:~/go/bin"
 export GOPATH="/root/go"
 export GOROOT="/usr/local/go"
 export GOCACHE="/root/.cache/go-build"
 export HOME="/root"
-echo -e "====== Ending golang install - $DATE ======\n" >> $LOG_FILE 2>&1
+echo "====== Ending golang install - ${DATE} ======" >> ${LOG_FILE} 2>&1
 
 ## For loop to easy installation
-echo "====== Starting packages install - $DATE ======" >> $LOG_FILE 2>&1
+echo "====== Starting packages install - ${DATE} ======" >> ${LOG_FILE} 2>&1
 {
+    $amass
     $anew
+    wget $antiburl -O /tmp/main.go && cd /tmp && go build main.go && mv main $HOME/go/bin/anti-burl && rm -f main.go
+    $arjun
     $assetfinder
+    $axiom
+    $chaos_client
+    $dalfox
+    $dirsearch && cd ${TOOLS_DIR}/dirsearch && pip3 install -r requirements.txt && chmod +x dirsearch.py
     $dnsx
     $ffuf
+    wget $findomain -O /tmp/findomain && chmod +x /tmp/findomain && mv findomain /usr/local/bin/
+    $gau
+    $gauplus
+    $gf
+    $gitdorker && cd ${TOOLS_DIR}/gitdorker && pip3 install -r requirements.txt && chmod +x GitDorker.py
+    $gitdumper
+    $gitscanner && chmod +x ${TOOLS_DIR}/gitscanner/gitscanner.sh
+    $githubsearch && cd ${TOOLS_DIR}/githubsearch && pip3 install -r requirements3.txt && chmod +x github-subdomains.py
     $gospider
+    $gowitness
     $hakrawler
+    $haktrails
+    $httprobe
     $httpx
     $interactsh_client
     $interactsh_server
     $nikto
     $jaeles
+    $jsscanner && cd ${TOOLS_DIR}/jsscanner && pip3 install -r requirements.txt && chmod +x JSScanner.py
     $notify
     $nuclei
     $nuclei_templates
     $proxify
     $subfinder
     $waybackurls
-} >> $LOG_FILE 2>&1
-ln -s $TOOLS_DIR/nikto/program/nikto.py /usr/local/bin/nikto
-echo "====== Ending packages install - $DATE ======" >> $LOG_FILE 2>&1
+} >> ${LOG_FILE} 2>&1
+
+## Create aliases
+ln -s ${TOOLS_DIR}/nikto/program/nikto.py /usr/local/bin/nikto
+ln -s ${TOOLS_DIR}/dirsearch/dirsearch.py /usr/local/bin/dirsearch
+ln -s ${TOOLS_DIR}/gitdorker/GitDorker.py /usr/local/bin/gitdorker
+ln -s ${TOOLS_DIR}/gitscanner/gitscanner.sh /usr/local/bin/gitscanner
+ln -s ${TOOLS_DIR}/githubsearch/github-subdomains.py /usr/local/bin/github-subdomains
+ln -s ${TOOLS_DIR}/jsscanner/JSScanner.py /usr/local/bin/jsscanner
+
+echo "====== Ending packages install - ${DATE} ======" >> ${LOG_FILE} 2>&1
 
 ## Finishing script
-echo "====== Finishing post install script - $DATE ======" >> $LOG_FILE 2>&1
+echo "====== Finishing post install script - ${DATE} ======" >> ${LOG_FILE} 2>&1
